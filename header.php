4be808b3a4d2b83339a2a2c586af8e20bdc6be98
Philip Curley <curleypg@gmail.com>
2017-08-30 19:48:58 -0400

    Remove header link

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
 			<?php
 			if (is_front_page()) :
 	    		echo do_shortcode("[metaslider id=85]");
