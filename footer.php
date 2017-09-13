1e3af6c1daf516978dbfb721767e9136b1524ca5
Philip Curley <curleypg@gmail.com>
2017-09-12 23:26:33 -0400

    Improve footer widget area

--- footer.php
+++ footer.php
@@ -11,21 +11,10 @@
 ?>
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
+		<div class="site-info container-flex-no-wrap container-justify-content">
 			<?php do_action( 'twentytwelve_credits' ); ?>
 				<!--<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>-->
-			<div style="float:left;width:40%;margin-bottom:20px;">
-				DTM Foundation is a 501(c)3 nonprofit organization.
-				<br />
-				Copyright © <?=date('Y')?> DTM Foundation
-			</div>
-			<?php
-			if (is_front_page() || is_page('24')) :
-			echo '<div style="float:right; text-align:right; width:40%; margin-bottom: 20px;">
-				A special thanks to <a href="mailto:curleypg@gmail.com">Philip Curley</a> & Scott M. Peters Photography for their hard work in the creation of this website.
-			</div>';
-			endif;
-			?>
+			<?php dynamic_sidebar( 'footer_widget' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function dtm_widgets_init() {
   register_sidebar( array(
     'name'           => 'Footer widgets',
     'id'            => 'footer_widget',
-    'before_widget' => '<div id="footer_widget">',
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
     'after_widget'  => '</div>',
     'before_title'  => '<h2 class="rounded">',
     'after_title'   => '</h2>',
